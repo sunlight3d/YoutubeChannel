d3cae9740a197a8efba30808a357111aea04439a
Nguyen Duc Hoang <sunlight4d@gmail.com>
2022-10-15 09:55:59 +0700

    add code

--- JavaFXTutorials2022/DemoApp/JavaFXApp/src/main/java/com/javafxdemo/javafxdemo/HelloController.java
+++ JavaFXTutorials2022/DemoApp/JavaFXApp/src/main/java/com/javafxdemo/javafxdemo/HelloController.java
@@ -5,10 +5,10 @@ import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label txtTitle;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        txtTitle.setText("ok, you pressed");
     }
 }
