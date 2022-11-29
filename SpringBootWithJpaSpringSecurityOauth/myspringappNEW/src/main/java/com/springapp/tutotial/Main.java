3d47a1ef0b00ddf445a03ab1c9c67f73bbfd1c5d
Nguyen Duc Hoang <sunlight4d@gmail.com>
2022-11-29 16:46:38 +0700

    add code

--- SpringBootWithJpaSpringSecurityOauth/myspringappNEW/src/main/java/com/springapp/tutotial/Main.java
+++ SpringBootWithJpaSpringSecurityOauth/myspringappNEW/src/main/java/com/springapp/tutotial/Main.java
@@ -2,12 +2,16 @@ package com.springapp.tutotial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
+mvn clean install
+Open Maven tab(right), Lifecycle, click clean, click install
+Output jar files in target/
+packages all dependencies mentioned in the pom file => jar.original
+java -jar target/tutotial-0.0.1-SNAPSHOT.jar --spring.profiles.active=test
+* */
 @SpringBootApplication
 public class Main {
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
-
 	}
-
 }
