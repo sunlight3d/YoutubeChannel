0ca6821ac87a1a8b015f11084de83256967202db
Nguyen Duc Hoang <sunlight4d@gmail.com>
2023-03-31 14:41:36 +0700

    add code

--- 2023/April/SpringBootMicroservice/microservices/order-service/src/main/java/com/myapp/orderservice/OrderserviceApplication.java
+++ 2023/April/SpringBootMicroservice/microservices/order-service/src/main/java/com/myapp/orderservice/OrderserviceApplication.java
@@ -2,7 +2,6 @@ package com.myapp.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class OrderserviceApplication {
 	public static void main(String[] args) {
@@ -10,3 +9,4 @@ public class OrderserviceApplication {
 	}
 
 }
+
